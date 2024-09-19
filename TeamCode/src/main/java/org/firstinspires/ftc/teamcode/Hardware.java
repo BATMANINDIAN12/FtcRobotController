9222a2060a1e81b263849f22bead741b751f08f5
BATMANINDIAN12 <125759883+BATMANINDIAN12@users.noreply.github.com>
2024-09-19 14:57:20 -0500

    More updating and add Motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -40,29 +40,68 @@ public class Hardware {
         frontLeft = hwMap.get(DcMotorEx.class,"frontLeft");
         rearRight = hwMap.get(DcMotorEx.class,"rearRight");
         rearLeft = hwMap.get(DcMotorEx.class,"rearLeft");
+
         liftLeft = hwMap.get(DcMotorEx.class,"liftLeft");
-        liftRight = hwMap.get(DcMotorEx.class, "Grabber Lift");
+        liftRight = hwMap.get(DcMotorEx.class, "liftRight");
+
+        liftRotationLeft = hwMap.get(DcMotorEx.class, "liftRotationLeft");
+        liftRotationRight = hwMap.get(DcMotorEx.class, "lifeRotationRight");
+
+        wristRotation = hwMap.get(DcMotorSimple.class, "wristRotation");
+        wristAngle = hwMap.get(DcMotorSimple.class, "wristAngle");
+
+        armLeft = hwMap.get(DcMotorSimple.class, "armLeft");
+        armRight = hwMap.get(DcMotorSimple.class, "armRight");
+
         grabber = hwMap.get(DcMotorSimple.class, "Grabber");
 
+
+        //setPower
         frontRight.setPower(0);
         frontLeft.setPower(0);
         rearRight.setPower(0);
         rearRight.setPower(0);
+
         liftLeft.setPower(0);
         liftRight.setPower(0);
+
+        liftRotationRight.setPower(0);
+        liftRotationLeft.setPower(0);
+
+        wristRotation.setPower(0);
+        wristAngle.setPower(0);
+
+        armLeft.setPower(0);
+        armRight.setPower(0);
+
         grabber.setPower(0);
 
+        //setDirection
         frontRight.setDirection(DcMotorEx.Direction.FORWARD);
         frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
         rearRight.setDirection(DcMotorEx.Direction.FORWARD);
         rearLeft.setDirection(DcMotorEx.Direction.REVERSE);
-        liftLeft.setDirection(DcMotor.Direction.FORWARD);
 
+        liftLeft.setDirection(DcMotor.Direction.FORWARD);
+        liftRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        liftRotationLeft.setDirection(DcMotorEx.Direction.FORWARD);
+        liftRotationRight.setDirection(DcMotorEx.Direction.REVERSE);
+
+        wristRotation.setDirection(DcMotorSimple.Direction.FORWARD);
+        wristAngle.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        grabber.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        //encoderVsNoEncoder
         frontRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         frontLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         rearLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         rearRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        liftLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        liftLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        liftRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         liftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
